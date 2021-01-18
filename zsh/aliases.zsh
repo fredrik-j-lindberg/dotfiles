500b9111d08585f311926dc96092a0df913114df
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2021-01-18 21:05:46 +0100

    zsh: Add functions for opening a commit (ghc) or branch (ghb)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,10 +10,20 @@ alias fzfvimhidden='nvim $(find . | fzf)'
 
 ########## Begin Custom Functions
 function openGitRepo() {
+  local syntax="openGitRepo <originRemote>"
   local originRemote=$1
+  if [ -z "$1" ]; then
+    echo "Missing origin remote. Syntax: \n$syntax"
+    return
+  fi
+
   local baseUrl=$(echo $originRemote | cut -d ":" -f 1 | cut -d "@" -f 2)
   local repo=$(echo $originRemote | cut -d ":" -f 2 | cut -d "." -f 1)
-  open "https://$baseUrl/$repo"
+  local link="https://$baseUrl/$repo"
+  if [[ -n $2 ]]; then
+    link="$link/$2"
+  fi
+  open $link
 }
 
 function gh() {
@@ -27,7 +37,15 @@ function gh() {
   if [[ -z "$remoteUrl" ]]; then
     echo "Could not find remote url at this location. Make sure it is a git repo!"
   else
-    openGitRepo $remoteUrl
+    openGitRepo $remoteUrl $2
   fi
 }
+
+function ghc(){
+  gh $1 commit/$2
+}
+function ghb(){
+  gh $1 tree/$2
+}
+
 ########## End Custom Functions
