5bb51937f434f55d0a3c0e74a5e4bbf931c0eeac
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2021-01-18 22:54:05 +0100

    zsh: Add command parsing to 'gh' (now allows -rcb flags)
    r - remote name
    c - commit hash
    b - branch
    
    Remove the now irrelevant ghc and ghb functions.

--- zsh/scripts/open-in-github.zsh
+++ zsh/scripts/open-in-github.zsh
@@ -1,3 +1,80 @@
+function usage() {
+  echo "USAGE: gh [.] [-r <remote name>] [-c <commit hash>] [-b <branch name>]"
+  return
+}
+
+function gh() {
+  local useCurrentDir=false
+  if [[ $1 == "." ]]; then
+    useCurrentDir=true
+    shift 1
+  fi
+
+  TEMP=$(getopt rcbh $@)
+  if [ $? != 0 ]; then
+    usage
+    return
+  fi
+
+  local remoteName="origin"
+  local commit
+  local branch
+  local rest
+  for i; do
+    case "$i" in
+    -r)
+      remoteName=$2
+      shift 2
+      ;;
+
+    -c)
+      commit=$2
+      shift 2
+      ;;
+
+    -b)
+      branch=$2
+      shift 2
+      ;;
+
+    -h)
+      usage() return
+      ;;
+
+    --)
+      shift
+      break
+      ;;
+    esac
+  done
+
+  local remoteUrl=$(getRemoteUrl $useCurrentDir)
+  if [[ -z "$remoteUrl" ]]; then
+    echo "Could not find remote url at this location for remote '$remoteName'. Make sure it is a git repo!"
+    return
+  fi
+
+  if [[ -n $commit ]]; then
+    openGitRepo $remoteUrl commit/$commit
+  elif [[ -n $branch ]]; then
+    openGitRepo $remoteUrl tree/$branch
+  else
+    openGitRepo $remoteUrl
+  fi
+}
+
+function getRemoteUrl() {
+  local useCurrentDir=$1
+  local remoteUrl
+  if [[ $useCurrentDir == true ]]; then
+    remoteUrl=$(git config --get remote.$remoteName.url)
+  else
+    local dir=$(find $HOME/git -type d -exec test -e '{}/.git' ';' -print -prune | fzf)
+    remoteUrl=$(git config --file $dir/.git/config --get remote.$remoteName.url)
+  fi
+  echo $remoteUrl
+}
+
 function openGitRepo() {
   local syntax="openGitRepo <remoteUrl>"
   local remoteUrl=$1
@@ -13,30 +90,4 @@ function openGitRepo() {
     link="$link/$2"
   fi
   open $link
-}
-
-function gh() {
-  local remoteUrl
-  local remoteName="origin"
-  if [[ -n $2 ]]; then
-    remoteName=$2
-  fi
-  if [[ $1 == "." ]]; then
-    remoteUrl=$(git config --get remote.$remoteName.url)
-  else
-    local dir=$(find $HOME/git -type d -exec test -e '{}/.git' ';' -print -prune | fzf)
-    remoteUrl=$(git config --file $dir/.git/config --get remote.$remoteName.url)
-  fi
-  if [[ -z "$remoteUrl" ]]; then
-    echo "Could not find remote url at this location. Make sure it is a git repo!"
-  else
-    openGitRepo $remoteUrl $3
-  fi
-}
-
-function ghc(){
-  gh $1 $2 commit/$3
-}
-function ghb(){
-  gh $1 $2 tree/$3
 }
