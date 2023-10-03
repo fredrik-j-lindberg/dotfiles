963610cc8caeedf178925a6503a4a96253ae4169
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2023-10-03 08:37:29 +0200

    zsh: Add function for getting subscription messages

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,8 @@ alias etig="nvim ~/.tigrc"
 alias fzfvim='nvim $(fzf)'
 alias fzfvimhidden='nvim $(find . | fzf)'
 alias home="cd ~"
+
+# $1 = project name, $2 = subscription name, #3 = limit (number of messages. Very unreliable though... Might return less than limit despite there being more)
+getSubscriptionMessageJson() {
+  gcloud pubsub subscriptions pull --project $1 $2 --limit $3 --format="json" |  jq '.[] | .message.data |= @base64d' | jq '.message.data |= fromjson'
+}
